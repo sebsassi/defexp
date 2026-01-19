0da6a4f9e779a399e41b3980f83c673d0ca55ed9
seb <posadowskybreen@proton.me>
2026-01-19 17:03:26 +0200

    work on eloss fit

--- scripts/misc/eloss_fit.py
+++ scripts/misc/eloss_fit.py
@@ -2,53 +2,65 @@ import argparse
 import os
 import glob
 
+import tqdm
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 
 
-def fit_func(eloss, p0, r0, p1, e1, s1, p2, e2, s2, p3, e3, s3):
-    return (p0*np.exp(-r0*eloss)
-            + p1*np.exp(-(eloss - e1)**2/(2*s1**2))/s1
-            + p2*np.exp(-(eloss - e2)**2/(2*s2**2))/s2
-            + p3*np.exp(-(eloss - e3)**2/(2*s3**2))/s3)/np.sqrt(2*np.pi)
+def fit_func(eloss, r0, p1, e1, s1, p2, e2, s2):
+    p0 = 1.0 - p1 - p2
+    norm1 = 1.0/(s1*np.sqrt(2.0*np.pi))
+    norm2 = 1.0/(s2*np.sqrt(2.0*np.pi))
+    return (p0*r0*np.exp(-r0*eloss)
+            + p1*norm1*np.exp(-(eloss - e1)**2/(2*s1**2))
+            + p2*norm2*np.exp(-(eloss - e2)**2/(2*s2**2)))
 
 
-def fit_func_der(eloss, p0, r0, p1, e1, s1, p2, e2, s2, p3, e3, s3):
+def fit_func_der(eloss, r0, p1, e1, s1, p2, e2, s2):
+    p0 = 1.0 - p1 - p2
+    norm1 = 1.0/(s1*np.sqrt(2.0*np.pi))
+    norm2 = 1.0/(s2*np.sqrt(2.0*np.pi))
     return np.array([
-        -p0*eloss*np.exp(-r0*eloss),
-        np.exp(-(eloss - e1)**2/(2*s1**2))/s1,
-        np.exp(-(eloss - e1)**2/(2*s1**2))*(eloss - e1)/s1**2,
-        np.exp(-(eloss - e1)**2/(2*s1**2))*((eloss - e1)**2 - s1**2)/s1**4,
-        np.exp(-(eloss - e2)**2/(2*s2**2))/s2,
-        np.exp(-(eloss - e2)**2/(2*s2**2))*(eloss - e2)/s2**2,
-        np.exp(-(eloss - e2)**2/(2*s2**2))*((eloss - e2)**2 - s2**2)/s2**4,
-        np.exp(-(eloss - e3)**2/(2*s3**2))/s3,
-        np.exp(-(eloss - e3)**2/(2*s3**2))*(eloss - e3)/s3**2,
-        np.exp(-(eloss - e3)**2/(2*s3**2))*((eloss - e3)**2 - s3**2)/s3**4
+        p0*(1 - r0*eloss)*np.exp(-r0*eloss),
+        norm1*np.exp(-(eloss - e1)**2/(2*s1**2)) - np.exp(-r0*eloss),
+        p1*norm1*np.exp(-(eloss - e1)**2/(2*s1**2))*(eloss - e1)/s1**2,
+        p1*norm1*np.exp(-(eloss - e1)**2/(2*s1**2))*((eloss - e1)**2 - s1**2)/s1**3,
+        norm2*np.exp(-(eloss - e2)**2/(2*s2**2)) - np.exp(-r0*eloss),
+        p2*norm2*np.exp(-(eloss - e2)**2/(2*s2**2))*(eloss - e2)/s2**2,
+        p2*norm2*np.exp(-(eloss - e2)**2/(2*s2**2))*((eloss - e2)**2 - s2**2)/s2**3,
     ])
 
 
-def opt_func(params, eloss, histogram):
-    p0 = 1.0 - params[1] - params[4] - params[7]
+def opt_f_df(params, eloss, histogram):
+
     fit_value = fit_func(
             eloss,
-            p0,
             params[0],
             params[1], params[2], params[3],
-            params[4], params[5], params[6],
-            params[7], params[8], params[9])
-    f = np.sum((fit_value - histogram)**2)
+            params[4], params[5], params[6])
+    f = (1.0/histogram.size)*np.sum((fit_value - histogram)**2)
+
     d_fit = fit_func_der(
             eloss,
-            p0,
             params[0],
             params[1], params[2], params[3],
-            params[4], params[5], params[6],
-            params[7], params[8], params[9])
-    df = 2.0*np.sum((fit_value - histogram)*d_fit, axis=-1)
+            params[4], params[5], params[6])
+    df = (2.0/histogram.size)*np.sum((fit_value - histogram)*d_fit, axis=-1)
 
     return f, df
+
+
+def opt_f(params, eloss, histogram):
+
+    fit_value = fit_func(
+            eloss,
+            params[0],
+            params[1], params[2], params[3],
+            params[4], params[5], params[6])
+    f = (1.0/histogram.size)*np.sum((fit_value - histogram)**2)
+
+    return f
 
 
 def main():
@@ -68,27 +80,44 @@ def main():
         energies = data["energies"]
         eloss = data["eloss"]
         emax = np.max(eloss)
+        bounds = [
+            (0.0, np.inf),
+            (0.0, 1.0),
+            (3.0, 8.0),
+            (0.01, emax),
+            (0.0, 1.0),
+            (8.0, 25.0),
+            (0.01, emax)
+        ]
 
-        bounds = ([0,       0,  1,      0.1,    0,  1,      0.1,    0,  1,      0.1 ],
-                  [emax,    1,  emax,   emax,   1,  emax,   emax,   1,  emax,   emax])
-
-        ineq_const = {
+        ineq_constr = {
             "type": "ineq",
             "fun": lambda x: np.array([
                 1.0 - x[1] - x[4] - x[7],
                 x[5] - x[2],
                 x[8] - x[5]]),
             "jac": lambda x: np.array([
-                [0.0, -1.0,  0.0, 0.0, -1.0,  0.0, 0.0, -1.0, 0.0, 0.0],
-                [0.0,  0.0, -1.0, 0.0,  0.0,  1.0, 0.0,  0.0, 0.0, 0.0],
-                [0.0,  0.0,  0.0, 0.0,  0.0, -1.0, 0.0,  0.0, 1.0, 0.0]])
+                [0.0, -1.0,  0.0, 0.0, -1.0,  0.0, 0.0],
+                [0.0,  0.0, -1.0, 0.0,  0.0,  1.0, 0.0],
+                [0.0,  0.0,  0.0, 0.0,  0.0, -1.0, 0.0]])
         }
 
+        constraint_matrix = np.array([
+            [0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0],
+            [0.0, 0.0, -1.0, 0.0, 0.0,  1.0, 0.0]])
+
+        constraint_bound_low = np.array([0.0, 0.0])
+        constraint_bound_high = np.array([1.0, np.inf])
+
+        linear_constraints = opt.LinearConstraint(
+                constraint_matrix, constraint_bound_low, constraint_bound_high)
+
         params = []
-        for i in range(histogram.shape[0]):
-            res = opt.shgo(opt_func, args=(eloss, histogram[i]), bounds=bounds, workers=-1)
+        print(f"Processing {filename}")
+        for i in tqdm.tqdm(range(histogram.shape[0])):
+            # res = opt.shgo(opt_f_df, args=(eloss, histogram[i]), bounds=bounds, constraints=ineq_constr, options={"jac": True})
+            res = opt.shgo(opt_f, args=(eloss, histogram[i]), bounds=bounds, constraints=linear_constraints, minimizer_kwargs={"method": "COBYQA"})
             params.append(res.x)
-            init = res.x
 
         params = np.array(params)
         histogram_fit = np.array([[fit_func(eloss[i], *params[j]) for i in range(histogram.shape[1])] for j in range(histogram.shape[0])])
@@ -97,17 +126,14 @@ def main():
         ax[0].imshow(histogram.T, aspect="auto")
         ax[1].imshow(histogram_fit.T, aspect="auto")
 
-        line0, = ax[2].plot(energies, params[:,0], label="$p_0$", linestyle="-")
-        ax[2].plot(energies, params[:,1], label="$\\tau$", color=line0.get_color(), linestyle="--")
-        line1, = ax[2].plot(energies, params[:,2], label="$p_1$", linestyle="-")
-        ax[2].plot(energies, params[:,3], label="$E_1$", color=line1.get_color(), linestyle="--")
-        ax[2].plot(energies, params[:,4], label="$\\sigma_1$", color=line1.get_color(), linestyle=":")
-        line2, = ax[2].plot(energies, params[:,5], label="$p_2$", linestyle="-")
-        ax[2].plot(energies, params[:,6], label="$E_2$", color=line2.get_color(), linestyle="--")
-        ax[2].plot(energies, params[:,7], label="$\\sigma_2$", color=line2.get_color(), linestyle=":")
-        line3, = ax[2].plot(energies, params[:,8], label="$p_3$", linestyle="-")
-        ax[2].plot(energies, params[:,9], label="$E_3$", color=line3.get_color(), linestyle="--")
-        ax[2].plot(energies, params[:,10], label="$\\sigma_3$", color=line3.get_color(), linestyle=":")
+        line0, = ax[2].plot(energies, params[:,0], label="$\\tau$", color="white", linestyle="--")
+        line1, = ax[2].plot(energies, params[:,1], label="$p_1$", linestyle="-")
+        ax[2].plot(energies, params[:,2], label="$E_1$", color=line1.get_color(), linestyle="--")
+        ax[2].plot(energies, params[:,3], label="$\\sigma_1$", color=line1.get_color(), linestyle=":")
+        line2, = ax[2].plot(energies, params[:,4], label="$p_2$", linestyle="-")
+        ax[2].plot(energies, params[:,5], label="$E_2$", color=line2.get_color(), linestyle="--")
+        ax[2].plot(energies, params[:,6], label="$\\sigma_2$", color=line2.get_color(), linestyle=":")
+        plt.legend()
         plt.show()
 
 
