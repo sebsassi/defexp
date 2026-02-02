f267e62cea07adf4c5a52aae5008b62267445924
seb <posadowskybreen@proton.me>
2026-02-02 23:05:24 +0200

    global fit

--- scripts/misc/eloss_fit.py
+++ scripts/misc/eloss_fit.py
@@ -63,6 +63,41 @@ def opt_f(params, eloss, histogram):
     return f
 
 
+def global_fit_func(eloss, energies, r0, p0a, p0b, p0c, p0d, p0e, p1a, p1b, p1c, p1d, p1e, e1a, e1b, s1a, s1b, p2a, p2b, p2c, p2d, p2e, e2a, e2b, s2a, s2b):
+    x = energies[np.newaxis,:]/energies[-1]
+    u0 = np.exp(-(p0a + x*(p0b + x*(p0c + x*(p0d + x*p0e)))))
+    u1 = np.exp(-(p1a + x*(p1b + x*(p1c + x*(p1d + x*p1e)))))
+    u2 = np.exp(-(p2a + x*(p2b + x*(p2c + x*(p2d + x*p2e)))))
+    pnorm = 1.0/(u0 + u1 + u2)
+    p0 = u0*pnorm
+    p1 = u1*pnorm
+    p2 = u2*pnorm
+    e1 = e1a + x*e1b
+    s1 = s1a + x*s1b
+    e2 = e2a + x*e2b
+    s2 = s2a + x*s2b
+    norm1 = 1.0/(s1*np.sqrt(2.0*np.pi))
+    norm2 = 1.0/(s2*np.sqrt(2.0*np.pi))
+    return (p0*r0*np.exp(-r0*eloss[:,np.newaxis])
+        + p1*norm1*np.exp(-(eloss[:,np.newaxis] - e1)**2/(2*s1**2))
+        + p2*norm2*np.exp(-(eloss[:,np.newaxis] - e2)**2/(2*s2**2)))
+
+
+def opt_global_f(params, eloss, energies, histogram):
+    fit_value = global_fit_func(
+            eloss,
+            params[0],
+            params[1], params[2], params[3], params[4], params[5],
+            params[6], params[7], params[8], params[9], params[10],
+            params[11], params[12],
+            params[13], params[14],
+            params[15], params[16], params[17], params[18], params[19],
+            params[20], params[21],
+            params[22], params[23])
+    f = (1.0/histogram.size)*np.sum((fit_value - histogram)**2)
+    return f
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("material", type=str, help="material label")
@@ -82,58 +117,40 @@ def main():
         emax = np.max(eloss)
         bounds = [
             (0.5, 100.0),
-            (0.0, 1.0),
-            (3.0, 8.0),
-            (0.01, emax),
-            (0.0, 1.0),
-            (8.0, 25.0),
-            (0.01, emax)
+            (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0),
+            (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0),
+            (3.0, 8.0), (-3.0, 3.0),
+            (0.01, emax), (-emax, emax),
+            (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0), (-10.0, 10.0),
+            (8.0, 25.0), (-8.0, 8.0),
+            (0.01, emax), (-emax, emax)
         ]
 
-        ineq_constr = {
-            "type": "ineq",
-            "fun": lambda x: np.array([
-                1.0 - x[1] - x[4] - x[7],
-                x[5] - x[2],
-                x[8] - x[5]]),
-            "jac": lambda x: np.array([
-                [0.0, -1.0,  0.0, 0.0, -1.0,  0.0, 0.0],
-                [0.0,  0.0, -1.0, 0.0,  0.0,  1.0, 0.0],
-                [0.0,  0.0,  0.0, 0.0,  0.0, -1.0, 0.0]])
-        }
+        res = opt.shgo(opt_global_f, args=(eloss, energies, histogram), bounds=bounds)
+        print(res.x)
 
-        constraint_matrix = np.array([
-            [0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0],
-            [0.0, 0.0, -1.0, 0.0, 0.0,  1.0, 0.0]])
+        # params = []
+        # print(f"Processing {filename}")
+        # for i in tqdm.tqdm(range(histogram.shape[0])):
+        #     res = opt.shgo(opt_f, args=(eloss[:-1], histogram[i]), bounds=bounds, constraints=linear_constraints, minimizer_kwargs={"method": "COBYQA"})
+        #     params.append(res.x)
 
-        constraint_bound_low = np.array([0.0, 0.0])
-        constraint_bound_high = np.array([1.0, np.inf])
-
-        linear_constraints = opt.LinearConstraint(
-                constraint_matrix, constraint_bound_low, constraint_bound_high)
-
-        params = []
-        print(f"Processing {filename}")
-        for i in tqdm.tqdm(range(histogram.shape[0])):
-            # res = opt.shgo(opt_f_df, args=(eloss, histogram[i]), bounds=bounds, constraints=ineq_constr, options={"jac": True})
-            res = opt.shgo(opt_f, args=(eloss[:-1], histogram[i]), bounds=bounds, constraints=linear_constraints, minimizer_kwargs={"method": "COBYQA"})
-            params.append(res.x)
-
-        params = np.array(params)
-        histogram_fit = np.array([[fit_func(eloss[i], *params[j]) for i in range(histogram.shape[1])] for j in range(histogram.shape[0])])
+        # params = np.array(params)
+        histogram_fit = global_fit_func(eloss, energies, *params)
+        # histogram_fit = np.array([[fit_func(eloss[i], *params[j]) for i in range(histogram.shape[1])] for j in range(histogram.shape[0])])
 
         fig, ax = plt.subplots(3)
         ax[0].imshow(histogram.T, aspect="auto")
         ax[1].imshow(histogram_fit.T, aspect="auto")
 
-        line0, = ax[2].plot(energies[:-1], params[:,0], label="$\\tau$", color="white", linestyle="--")
-        line1, = ax[2].plot(energies[:-1], params[:,1], label="$p_1$", linestyle="-")
-        ax[2].plot(energies[:-1], params[:,2], label="$E_1$", color=line1.get_color(), linestyle="--")
-        ax[2].plot(energies[:-1], params[:,3], label="$\\sigma_1$", color=line1.get_color(), linestyle=":")
-        line2, = ax[2].plot(energies[:-1], params[:,4], label="$p_2$", linestyle="-")
-        ax[2].plot(energies[:-1], params[:,5], label="$E_2$", color=line2.get_color(), linestyle="--")
-        ax[2].plot(energies[:-1], params[:,6], label="$\\sigma_2$", color=line2.get_color(), linestyle=":")
-        plt.legend()
+        # line0, = ax[2].plot(energies[:-1], params[:,0], label="$\\tau$", color="white", linestyle="--")
+        # line1, = ax[2].plot(energies[:-1], params[:,1], label="$p_1$", linestyle="-")
+        # ax[2].plot(energies[:-1], params[:,2], label="$E_1$", color=line1.get_color(), linestyle="--")
+        # ax[2].plot(energies[:-1], params[:,3], label="$\\sigma_1$", color=line1.get_color(), linestyle=":")
+        # line2, = ax[2].plot(energies[:-1], params[:,4], label="$p_2$", linestyle="-")
+        # ax[2].plot(energies[:-1], params[:,5], label="$E_2$", color=line2.get_color(), linestyle="--")
+        # ax[2].plot(energies[:-1], params[:,6], label="$\\sigma_2$", color=line2.get_color(), linestyle=":")
+        # plt.legend()
         plt.show()
 
 
